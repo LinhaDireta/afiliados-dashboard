bbfe5115d727edae900d9e420578b05d67debb33
João Teixeira <joao@ycloud.com.br>
2018-09-27 19:27:44 -0300

    Progresso da listagem de locais

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://api-final.linhadiretapm.com/api',
+  api_url: '//api-final.linhadiretapm.com/api',
   GOOGLE_KEY: "AIzaSyC2s_mnFI_Wdv-nJz09sOWgfdVdnXQedI8"
 };
